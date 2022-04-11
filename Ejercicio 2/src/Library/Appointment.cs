a3e12c2b812dc721f06c2eb59f10e9d4f63ff300
Juan-Tabarez <101071966+Juan-Tabarez@users.noreply.github.com>
2022-04-11 14:47:27 -0300

    Update Appointment.cs
    Se pusheo mal.

--- Ejercicio 2/src/Library/Appointment.cs
+++ Ejercicio 2/src/Library/Appointment.cs
@@ -5,10 +5,11 @@ namespace Library
     public class Appointment
     {
         public readonly string AppointmentPlace;
+        
         public DateTime Date;
+        
         public readonly long AppointmentId;
 
-        
         //Lleva la cuenta de todas las Appointment creadas.
         private static long AppointmentsCreated = 0;
 
@@ -18,20 +19,6 @@ namespace Library
             this.Date = date;
             AppointmentsCreated++;
             this.AppointmentId = AppointmentsCreated;
-
-        public readonly bool isValidAppointment;
-
-        //Lleva la cuenta de todas las Appointment creadas.
-        private static long AppointmentNumber = 0;
-
-        public Appointment(string appointmentPlace, DateTime date)
-        {
-            if (!string.IsNullOrEmpty(appointmentPlace))
-            this.AppointmentPlace = appointmentPlace;
-            this.Date = date;
-            AppointmentNumber++;
-            this.AppointmentId = AppointmentNumber;
-            this.isValidAppointment = true;
         }
     }
 }
